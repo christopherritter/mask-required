2c03e5d476b012b8a91d3716d566cdf4186d0431
Christopher Ritter <chris@christopherritter.com>
2020-07-06 18:48:45 -0400

    initial commit

---
+++ src/firebase.js
@@ -0,0 +1,35 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+// firebase init - add your own config here
+const firebaseConfig = {
+  apiKey: "AIzaSyCHzVbXJqB_-tUwT2AeUGZ_-HRkvJOHjqI",
+  authDomain: "mask-required.firebaseapp.com",
+  databaseURL: "https://mask-required.firebaseio.com",
+  projectId: "mask-required",
+  storageBucket: "mask-required.appspot.com",
+  messagingSenderId: "783326866906",
+  appId: "1:783326866906:web:80587a7cde78e644f924e3",
+};
+firebase.initializeApp(firebaseConfig);
+
+// utils
+const db = firebase.firestore();
+const auth = firebase.auth();
+
+// collection references
+const usersCollection = db.collection("users");
+const postsCollection = db.collection("posts");
+const commentsCollection = db.collection("comments");
+const likesCollection = db.collection("likes");
+
+// export utils/refs
+export {
+  db,
+  auth,
+  usersCollection,
+  postsCollection,
+  commentsCollection,
+  likesCollection,
+};
