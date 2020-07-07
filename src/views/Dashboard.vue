4dd1fe4103bc7bd2491821533da2f283d22d5ac8
Christopher Ritter <chris@christopherritter.com>
2020-07-07 16:01:56 -0400

    Added moment to format dates

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -24,8 +24,8 @@
         <div v-if="posts.length">
           <div v-for="post in posts" :key="post.id" class="post">
             <h5>{{ post.userName }}</h5>
-            <span>{{ post.createdOn }}</span>
-            <p>{{ post.content }}</p>
+            <span>{{ post.createdOn | formatDate }}</span>
+            <p>{{ post.content | trimLength }}</p>
             <ul>
               <li>
                 <a>comments {{ post.comments }}</a>
@@ -47,6 +47,7 @@
 
 <script>
 import { mapState } from "vuex";
+import moment from "moment";
 
 export default {
   data() {
@@ -63,6 +64,22 @@ export default {
     createPost() {
       this.$store.dispatch("createPost", { content: this.post.content });
       this.post.content = "";
+    },
+  },
+  filters: {
+    formatDate(val) {
+      if (!val) {
+        return "-";
+      }
+
+      let date = val.toDate();
+      return moment(date).fromNow();
+    },
+    trimLength(val) {
+      if (val.length < 200) {
+        return val;
+      }
+      return `${val.substring(0, 200)}...`;
     },
   },
 };
