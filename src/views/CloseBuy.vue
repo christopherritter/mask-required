04d05608ae33bfeb6a01a02d968107ba5db18c81
Christopher Ritter <chris@christopherritter.com>
2020-07-12 10:11:58 -0400

    Finished CloseBuy view; packaged for deployment

--- src/views/CloseBuy.vue
+++ src/views/CloseBuy.vue
@@ -156,6 +156,8 @@ export default {
         }
       );
 
+      const infoWindow = new google.maps.InfoWindow();
+
       for (let i = 0; i < this.places.length; i++) {
         const lat = this.places[i].geometry.location.lat;
         const lng = this.places[i].geometry.location.lng;
@@ -163,6 +165,13 @@ export default {
         const marker = new google.maps.Marker({
           position: new google.maps.LatLng(lat, lng), 
           map: map
+        });
+
+        google.maps.event.addListener(marker, "click", () => {
+          infoWindow.setContent(
+            `<div class="ui header">${this.places[i].name}</div>`
+          );
+          infoWindow.open(map, marker);
         });
       }
     }
