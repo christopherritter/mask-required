8d2b09d94010a80936894e6bd8828b1de2e167f3
Christopher Ritter <chris@christopherritter.com>
2020-07-27 19:15:53 -0400

    Built for produciton.

--- src/store/index.js
+++ src/store/index.js
@@ -407,6 +407,8 @@ const store = new Vuex.Store({
           var token = result.credential.accessToken;
           // The signed-in user info.
           var user = result.user;
+          console.log("Signing in with Facebook provider.")
+          console.log(result);
           dispatch("fetchUserProfile", user);
         })
         .catch(function(error) {
