650de1a6572dbdc02ac9c9a74da062d18e8897f8
Christopher Ritter <chris@christopherritter.com>
2020-07-30 16:33:03 -0400

    Built for production.

--- dist/js/nearby.09cbf557.js
+++ dist/js/nearby.09cbf557.js
@@ -1,2 +1,2 @@
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["nearby"],{6384:function(n,a,e){"use strict";e.r(a);var o=function(){var n=this,a=n.$createElement,e=n._self._c||a;return e("v-main",[e("v-container",[e("v-row",[e("v-col",[e("h1",[n._v("Nearby")])])],1)],1)],1)},r=[],t=e("2877"),c=e("6544"),l=e.n(c),i=e("62ad"),u=e("a523"),s=e("f6c4"),w=e("0fd9"),v={},f=Object(t["a"])(v,o,r,!1,null,null,null);a["default"]=f.exports;l()(f,{VCol:i["a"],VContainer:u["a"],VMain:s["a"],VRow:w["a"]})}}]);
-//# sourceMappingURL=nearby.951342e2.js.map
+//# sourceMappingURL=nearby.09cbf557.js.map
