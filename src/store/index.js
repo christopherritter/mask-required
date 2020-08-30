0e57c68ee659b2881bfe4549b642f3979ba9e3c6
Christopher Ritter <chris@christopherritter.com>
2020-08-30 13:01:30 -0400

    Added address component to places.

--- src/store/index.js
+++ src/store/index.js
@@ -743,7 +743,7 @@ const store = new Vuex.Store({
     },
     async createPlace({ state, getters }, place) {
       var apiKey = getters.getFixieKey;
-      const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${place.place_id}&fields=formatted_address,geometry,icon,name,place_id,plus_code,types&key=${apiKey}`;
+      const URL = `https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=${place.place_id}&fields=formatted_address,geometry,icon,name,place_id,plus_code,types,address_component&key=${apiKey}`;
       var newPlace = {};
       // console.log(URL)
 
