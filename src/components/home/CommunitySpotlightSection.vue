62d8946393b5653945a15661241169613a4862ee
Christopher Ritter <chris@christopherritter.com>
2020-09-13 12:40:43 -0400

    Added common header.

--- src/components/home/CommunitySpotlightSection.vue
+++ src/components/home/CommunitySpotlightSection.vue
@@ -1,9 +1,15 @@
 <template>
   <section id="community-spotlight-section">
     <v-row>
+      <v-col class="d-flex align-center"
+        ><v-divider class="mb-1"></v-divider
+      ></v-col>
       <v-col>
-        <h3>Community Spotlight</h3>
+        <h3 class="text-center">Community Spotlight</h3>
       </v-col>
+      <v-col class="d-flex align-center"
+        ><v-divider class="mb-1"></v-divider
+      ></v-col>
     </v-row>
   </section>
 </template>
--- src/components/home/LatestReviewsSection.vue
+++ src/components/home/LatestReviewsSection.vue
@@ -1,9 +1,15 @@
 <template>
   <section id="latest-reviews-section">
     <v-row>
+      <v-col class="d-flex align-center"
+        ><v-divider class="mb-1"></v-divider
+      ></v-col>
       <v-col>
-        <h3>Latest Reviews</h3>
+        <h3 class="text-center">Latest Reviews</h3>
       </v-col>
+      <v-col class="d-flex align-center"
+        ><v-divider class="mb-1"></v-divider
+      ></v-col>
     </v-row>
   </section>
 </template>
