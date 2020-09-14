2b4fe553fa91c7161fbe7e4aa152e3b29f749293
Christopher Ritter <chris@christopherritter.com>
2020-09-14 15:51:44 -0400

    Cleaned up comments.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,25 +3,28 @@
     <v-container>
       <address-search-bar></address-search-bar>
       <primary-search-bar></primary-search-bar>
-      <!-- <popular-type-section></popular-type-section>
+      <popular-type-section></popular-type-section>
       <latest-reviews-section></latest-reviews-section>
-      <community-spotlight-section></community-spotlight-section> -->
+      <community-spotlight-section></community-spotlight-section>
     </v-container>
   </v-main>
 </template>
 
 <script>
 import AddressSearchBar from "@/components/home/AddressSearchBar";
-// import CommunitySpotlightSection from "@/components/home/CommunitySpotlightSection";
-// import LatestReviewsSection from "@/components/home/LatestReviewsSection";
-// import PopularTypeSection from "@/components/home/PopularTypeSection";
+import CommunitySpotlightSection from "@/components/home/CommunitySpotlightSection";
+import LatestReviewsSection from "@/components/home/LatestReviewsSection";
+import PopularTypeSection from "@/components/home/PopularTypeSection";
 import PrimarySearchBar from "@/components/home/PrimarySearchBar";
 
 export default {
   name: "Home",
   components: {
     AddressSearchBar,
+    CommunitySpotlightSection,
+    LatestReviewsSection,
     PrimarySearchBar,
+    PopularTypeSection
   },
 };
 </script>
