66bf96d9f40d82b4cfa82a5ff155be9f70111757
Christopher Ritter <chris@christopherritter.com>
2020-09-15 15:15:42 -0400

    Added details to Popular Types cards.

--- src/components/home/PopularTypeSection.vue
+++ src/components/home/PopularTypeSection.vue
@@ -1,23 +1,109 @@
 <template>
   <section id="popular-type-section">
     <v-row>
-      <v-col class="d-flex align-center"
-        ><v-divider class="mb-1"></v-divider
-      ></v-col>
-      <v-col>
-        <h3 class="text-center">Popular {{ type.name + "s" | replaceUnderscore }}</h3>
+      <v-col class="d-flex align-center">
+        <v-divider class="mb-1"></v-divider>
       </v-col>
-      <v-col class="d-flex align-center"
-        ><v-divider class="mb-1"></v-divider
-      ></v-col>
+      <v-col>
+        <h3 class="text-center">
+          Popular {{ (type.name + "s") | replaceUnderscore }}
+        </h3>
+      </v-col>
+      <v-col class="d-flex align-center">
+        <v-divider class="mb-1"></v-divider>
+      </v-col>
     </v-row>
     <v-row>
-      <v-col cols="12" sm="12" md="4" lg="3" style="font-size: 1.25rem">Here are some popular {{ type.name  + "s" | replaceUnderscore }} that people are currently searching for.</v-col>
+      <v-col cols="12" sm="12" md="4" lg="3" style="font-size: 1.25rem"
+        >Here are some popular {{ (type.name + "s") | replaceUnderscore }} that
+        people are currently searching for.</v-col
+      >
       <v-col cols="12" sm="12" md="8" lg="9">
         <v-row>
-          <v-col cols="12" sm="6" md="6" lg="4" v-for="place in placesToDisplay" :key="place.place_id">
-            <v-card height="200">
-              <v-card-title>{{ place.name }}</v-card-title>
+          <v-col
+            cols="12"
+            sm="6"
+            md="6"
+            lg="4"
+            v-for="place in placesToDisplay"
+            :key="place.place_id"
+          >
+            <v-card
+              height="200"
+              outlined
+              @mouseover.native="highlightedCard = place.place_id"
+              @mouseleave.native="highlightedCard = null"
+              :style="styleObject"
+              :class="{
+                'teal white--text': highlightedCard == place.place_id,
+              }"
+              style="cursor: pointer"
+              @click="viewPlace(place)"
+            >
+              <v-card-title
+                :class="{
+                  'white--text': highlightedCard == place.place_id,
+                }"
+              >
+                {{ place.name | truncateWithEllipse(18) }}
+              </v-card-title>
+              <v-card-subtitle
+                class="pb-2"
+                :class="{
+                  'white--text': highlightedCard == place.place_id,
+                }"
+              >
+                {{ place.formatted_address }}
+              </v-card-subtitle>
+              <div
+                v-if="place.reviews && place.reviews.length > 0"
+                class="d-flex flex-row"
+              >
+                <v-rating
+                  v-model="place.ratings.general"
+                  background-color="yellow"
+                  color="yellow accent-4"
+                  length="5"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+                  readonly
+                  class="ml-4 mb-1"
+                ></v-rating>
+                <span class="text-caption mt-1 ml-2">
+                  {{ place.reviews.length }} reviews
+                </span>
+              </div>
+              <div v-else class="d-flex flex-row">
+                <v-rating
+                  background-color="gray"
+                  color="gray accent-4"
+                  length="5"
+                  dense
+                  half-increments
+                  hover
+                  size="18"
+                  readonly
+                  class="ml-4 mb-1"
+                ></v-rating>
+                <span class="text-caption mt-1 ml-2">
+                  0 reviews
+                </span>
+              </div>
+              <v-card-text
+                v-if="
+                  place.reviews &&
+                    place.reviews.length > 0 &&
+                    place.reviews[0].title.length > 0
+                "
+                class="pt-2 hidden-sm-and-down"
+                :class="{
+                  'white--text': highlightedCard == place.place_id,
+                }"
+              >
+                &ldquo;{{ place.reviews[0].title }}&rdquo;
+              </v-card-text>
             </v-card>
           </v-col>
         </v-row>
@@ -78,7 +164,7 @@ export default {
       if (this.types.length > 0) {
         this.loading = false;
       }
-    }
+    },
   },
   filters: {
     replaceUnderscore(val) {
