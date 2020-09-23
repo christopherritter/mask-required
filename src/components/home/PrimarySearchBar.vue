1dcecadc64db45c715af0beb8b9b6536965c8453
Christopher Ritter <chris@christopherritter.com>
2020-09-23 11:25:53 -0400

    Added geocode to primary search bar options.

--- src/components/home/PrimarySearchBar.vue
+++ src/components/home/PrimarySearchBar.vue
@@ -43,7 +43,7 @@ export default {
   data: () => ({
     currentLocation: null,
     options: {
-      types: ["establishment"],
+      types: ["establishment", "geocode"],
       componentRestrictions: { country: "us" },
       fields: [
         // "formatted_address",
