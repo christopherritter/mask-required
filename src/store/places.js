edf14a69f7c065775d3d5cce9602a14351ba561d
Christopher Ritter <chris@christopherritter.com>
2020-09-26 20:05:49 -0400

    Added places data for default store.

---
+++ src/store/places.js
@@ -0,0 +1,19 @@
+const places = [
+  {
+    address_components: [],
+    coordinates: [],
+    createdOn: new Date(),
+    formatted_address: "",
+    g: {},
+    geohash: "",
+    geometry: {},
+    icon: "",
+    name: "",
+    place_id: "",
+    plus_code: {},
+    review: 0,
+    types: []
+  }
+]
+
+export default places;
